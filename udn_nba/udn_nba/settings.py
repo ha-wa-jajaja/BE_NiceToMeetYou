7d27e2a814e135ecc8da5a280003509277a03542
ha-wa-jajaja <wtfuhell@gmail.com>
2025-04-13 20:56:02 +0800

    enable doc

--- udn_nba/udn_nba/settings.py
+++ udn_nba/udn_nba/settings.py
@@ -139,9 +139,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
     ],

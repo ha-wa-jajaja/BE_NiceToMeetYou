b518a2505336b0358f1b0ac98064bfb61845a1ba
ha-wa-jajaja <wtfuhell@gmail.com>
2025-04-14 16:49:23 +0800

    Tag endpoint testing

--- udn_nba/tags/views.py
+++ udn_nba/tags/views.py
@@ -5,6 +5,7 @@ from drf_spectacular.utils import (
     extend_schema_view,
 )
 from rest_framework import mixins, viewsets
+from rest_framework.exceptions import ValidationError
 from tags.models import Tag
 from tags.serializers import TagDetailSerializer, TagSerializer
 
@@ -17,8 +18,13 @@ from tags.serializers import TagDetailSerializer, TagSerializer
                 OpenApiTypes.STR,
                 description="Name of the tag to filter",
             ),
-        ]
-    )
+            OpenApiParameter(
+                "type",
+                OpenApiTypes.STR,
+                description="Type of the tag to filter",
+            ),
+        ],
+    ),
 )
 class TagViewSet(
     mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
@@ -27,15 +33,26 @@ class TagViewSet(
 
     queryset = Tag.objects.all()
     serializer_class = TagDetailSerializer
-    lookup_field = "name"
 
     def get_queryset(self):
         """Retrieve tags."""
 
         name = self.request.query_params.get("name")
+        tag_type = self.request.query_params.get("type")
         queryset = self.queryset
+
+        #  Check if both filters are provided
+        if name and tag_type:
+            raise ValidationError(
+                {
+                    "error": "Only one filter parameter (name or type) can be used at a time."
+                }
+            )
+
         if name:
-            queryset = queryset.filter(title__icontains=name)
+            queryset = queryset.filter(name__icontains=name)
+        elif tag_type:
+            queryset = queryset.filter(type=tag_type)
 
         return queryset.distinct()
 
